e572c061c38a1663dfc9f35c8c126d418dc2dbb0
Vitória Canon <98993613+canonvitoria@users.noreply.github.com>
2024-05-11 18:04:22 -0300

    Aula-89

--- Seção8/Aula-82/terminal.js
+++ Seção8/Aula-82/terminal.js
@@ -1,2 +1,56 @@
 // XMLHttpsRequest (GET) + Promises
 
+//xml = XMLHttpsRequest
+//request = função que vai buscar algumas páginas
+
+
+const request  = obj => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+        //Verficação
+        if (xhr.status >= 200 && xhr.status < 300) {
+            obj.success(xhr.responseText);
+        } else {
+            obj.error(xhr.statusText);
+        }
+    })
+   
+    //Se fosse metodo POST, recebendo de um formulário
+    //xhr.send('dados do formulario')
+}
+
+//Capturar os eventos dos cliques
+document.addEventListener('click', e => {
+    const element = e.target;
+    const tag = element.tagName.toLowerCase();
+
+    //Checkar se a tag seria um <a>
+    if(tag === 'a') {
+        //Previne o evento
+        e.preventDefault();
+        carregaPagina(element);
+    }
+})
+
+function carregaPagina(element) {
+    const href = element.getAttribute('href');
+
+    request({
+        method: 'GET',
+        url: href,
+        success(response) {
+            carregaResultado(response);
+        },
+        error(errorText) {
+            console.log(errorText);
+        }
+    })
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}

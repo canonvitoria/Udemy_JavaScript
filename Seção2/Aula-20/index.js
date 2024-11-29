83e35a886108e52ce1bbef3e065954231871c490
VitoriaCanon <vitoriacanon1212@gmail.com>
2024-11-29 17:57:09 -0300

    Aula 20 - Seção 2

---
+++ Seção2/Aula-20/index.js
@@ -0,0 +1,23 @@
+// Numbers no JavaScript
+
+let num1 = 1; //number
+let num2 = 2.5; //number
+
+//Conversão 
+console.log(num1.toString() + num2);
+
+//Alteração da Tipagem
+num1 = num1.toString();
+
+//Verficação da Tipagem
+console.log(typeof num1);
+
+//Representação Binária
+console.log(num1.toString(2));
+
+//Casas decimais 
+console.log(num1.toFixed(2));
+
+//Verficação de inteiro
+console.log(Number.isInteger(num1));
+
